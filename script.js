07a4a64732c9e24e60869a82e90a1f8db59c9c38
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-12-13 18:03:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 //your JS code here. If required.
-function playSound(soundFile) {
-  const audioElement = document.getElementById('audio');
+let myaudio=new Audio();
 
-  // Clear any existing source elements
-  while (audioElement.firstChild) {
-    audioElement.removeChild(audioElement.firstChild);
-  }
+function applause(){
+	myaudio.src= "./sounds/sound1";
+	myaudio.currentTime=0;
+	myaudio.play();
+	
+}
+function boo() {
+	myaudio.src="./sounds/sound2";
+	myaudio.currentTime=0;
+	myaudio.play();
+}
+function gasp() {
+	myaudio.src="./sounds/sound3";
+	myaudio.currentTime=0;
+	myaudio.play();
+}
+function tada() {
+	myaudio.src="./sounds/sound4";
+	myaudio.currentTime=0;
+	myaudio.play();
 
-  const sourceElement = document.createElement('source');
-  sourceElement.src = `sounds/${soundFile}`;
-  audioElement.appendChild(sourceElement);
-
-  audioElement.load();
-  audioElement.play()
-    .catch((error) => {
-      console.error(error);
-      alert('Failed to play the audio. Please check the file format and location.');
-    });
+}
+function victory() {
+	myaudio.src="./sounds/sound5";
+	myaudio.currentTime=0;
+	myaudio.play();
+}
+function wrong() {
+	myaudio.src="./sounds/sound6";
+	myaudio.currentTime=0;
+	myaudio.play();
+}
+function stop() {
+	myaudio.src="./sounds/sound7";
+	myaudio.currentTime=0;
+	myaudio.play();
 }
 
-function stopSound() {
-  const audioElement = document.getElementById('audio');
-  audioElement.pause();
-  audioElement.currentTime = 0;
+function stop() {
+	myaudio.pause();
+	
 }
